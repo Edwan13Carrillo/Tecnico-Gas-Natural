ea412b4aa2a43b80b3b06d9fce50d6bedbc4c56b
Edwan13Carrillo <edwan13alexander@gmail.com>
2026-03-29 21:28:50 -0500

    modelo

--- js/modelo.js
+++ js/modelo.js
@@ -1,5 +1,8 @@
 const url = "https://script.google.com/macros/s/AKfycbypkJwD0KbWyr2wYMBIDsFBlxbZ_GAjNR3MCCM8r__09GkozDtHWIWzG3JVIUgKWF2x3w/exec"
 
-$.get(url, function(data) {
-    $("#descripcionH").text(data[0].descripcion);
-}, "json");
+fetch(url)
+    .then(res => res.json())
+    .then(data => {
+        $("#descripcionH").text(data[0].descripcion);
+    })
+    .catch(err => console.error("Error:", err));
